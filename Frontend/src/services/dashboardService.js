73259aaafd541d43dd370354a9009459dff59af0
Cristina Astocaza <U23272489@utp.edu.pe>
2026-09-25 02:16:44 -0500

    feat(netlify): use browser demo storage

--- Frontend/src/services/dashboardService.js
+++ Frontend/src/services/dashboardService.js
@@ -1,6 +1,10 @@
-import api from './api';
+import { getCursos, getCategorias, getPreguntas, getExamenes } from './demoStore';
 
-export const obtenerStatsDashboard = async () => {
-  const { data } = await api.get('/dashboard/stats');
-  return data;
-};
+export const obtenerStatsDashboard = async () => ({
+  totalCursos: getCursos().length,
+  totalAreas: getCategorias().length,
+  totalCategorias: getCategorias().length,
+  totalPreguntas: getPreguntas().length,
+  totalExamenes: getExamenes().length,
+  examenesGenerados: getExamenes().length,
+});
